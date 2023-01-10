80f568c561cd958039747e25f5ebc838b8b463b0
pooichai <pooichai7@gmail.com>
2023-01-10 19:15:20 +0700

    Create footer.js

---
+++ footer.js
@@ -0,0 +1,19 @@
+document.write('<div id="floatads" style="width:100%;margin:auto; text-align:center;float:none;overflow:hidden; display:scroll;position:fixed; bottom:0;z-index:9999">');
+document.write('<div style="text-align:center;display:block;max-width:729px;height:auto;overflow:hidden;margin:auto">');
+
+//Paste Iklan Disini
+
+	atOptions = {
+		'key' : '00cd8e71a598d3131381d005535ef9fc',
+		'format' : 'iframe',
+		'height' : 90,
+		'width' : 728,
+		'params' : {}
+	};
+	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://www.effectivecreativeformat.com/00cd8e71a598d3131381d005535ef9fc/invoke.js"></scr' + 'ipt>');
+
+
+// Batas Iklan
+
+document.write('</div>');
+document.write('</div>');
