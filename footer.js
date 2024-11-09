37763c7ad49f276da856739fb2cfc7839236b0fb
pooichai <pooichai7@gmail.com>
2024-11-10 02:44:03 +0700

    Update footer.js

--- footer.js
+++ footer.js
@@ -1,16 +1,17 @@
-document.write('<div id="floatads" style="width:100%;margin:auto; text-align:center;float:none;overflow:hidden; display:scroll;position:fixed; bottom:0;z-index:9999">');
+document.write('<div id="floatads" style="width:100%;margin:auto; text-align:center;float:none;overflow:hidden; display:scroll;position:fixed; top:0;z-index:9999">');
 document.write('<div style="text-align:center;display:block;max-width:729px;height:auto;overflow:hidden;margin:auto">');
 
 //Paste Iklan Disini
 
-		atOptions = {
-		'key' : '00cd8e71a598d3131381d005535ef9fc',
+	atOptions = {
+		'key' : 'ea7073a5f88001ce3ea7a9725829313d',
 		'format' : 'iframe',
 		'height' : 90,
 		'width' : 728,
 		'params' : {}
 	};
-	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://canoemissioninjunction.com/00cd8e71a598d3131381d005535ef9fc/invoke.js"></scr' + 'ipt>');
+	document.write('<scr' + 'ipt type="text/javascript" src="//www.highperformanceformat.com/ea7073a5f88001ce3ea7a9725829313d/invoke.js"></scr' + 'ipt>');
+
 // Batas Iklan
 
 document.write('</div>');
