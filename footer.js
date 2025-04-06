e62f97a618c6c8b678d9735ba98275b6c4afccb5
pooichai <pooichai7@gmail.com>
2025-04-06 07:01:34 +0700

    Update footer.js

--- footer.js
+++ footer.js
@@ -10,7 +10,8 @@ document.write('<div style="text-align:center;display:block;max-width:729px;heig
 		'width' : 728,
 		'params' : {}
 	};
-	document.write('<scr' + 'ipt type="text/javascript" src="//www.highperformanceformat.com/ea7073a5f88001ce3ea7a9725829313d/invoke.js"></scr' + 'ipt>');
+</script>
+<script type="text/javascript" src="//www.highperformanceformat.com/ea7073a5f88001ce3ea7a9725829313d/invoke.js"></scr' + 'ipt>');
 
 // Batas Iklan
 
